0d73355c758a710384d582d031c67d301b2a2090
Alex Sohn <alex.sohn@sentry.io>
2025-08-28 11:22:54 -0400

    change dsn for sentry

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "http://a72cade0aa634ec0c73855c5f32aff85@dev.getsentry.net:8000/2",
+  dsn: "https://ea0a93e6703abdcec2f36b9323996cf4@o4509921934573568.ingest.us.sentry.io/4509922369863680",
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true
