58a6876a34a6faa5cf29919d7c625e6339748b1a
Alex Sohn <alex.sohn@sentry.io>
2025-08-28 11:14:56 -0400

    initial commit

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "http://a72cade0aa634ec0c73855c5f32aff85@dev.getsentry.net:8000/2",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
+
+createRoot(document.getElementById('root')).render(
+  <App />,
+)
