b72c24f7f96e3ebebed872d8763864d21c1cf09d
Alex Sohn <alex.sohn@sentry.io>
2025-09-02 13:37:32 -0400

    fix the config goddamn

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -16,7 +16,7 @@ Sentry.init({
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
   // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:/ / yourserver.io / api / ],
+  tracePropagationTargets: ["localhost"],
 });
 
 createRoot(document.getElementById('root')).render(
